9a8f737f3b686d72bd606c9d028b77454806ea94
TrinhDinhPhuc <phuccoi996@gmail.com>
2018-03-31 00:33:27 +0700

    update

--- SatHachBangLaiXe/Properties/Settings.Designer.cs
+++ SatHachBangLaiXe/Properties/Settings.Designer.cs
@@ -58,5 +58,16 @@ namespace SatHachBangLaiXe.Properties {
                 this["RememberMe"] = value;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=PHUCCOI\\PHUCCOI;Initial Catalog=SATHACHBANGLAI1;Integrated Security=T" +
+            "rue")]
+        public string SATHACHBANGLAI1ConnectionString {
+            get {
+                return ((string)(this["SATHACHBANGLAI1ConnectionString"]));
+            }
+        }
     }
 }
